1db55df955efe7a729ea27a79a5209b47370033b
Fernando Arias <fernandorafaelarias@gmail.com>
2020-02-24 13:53:19 -0300

    Part 2 - Challenge 4 solved

--- part-02/coding-challenge-4/challenge-04.js
+++ part-02/coding-challenge-4/challenge-04.js
@@ -1,1 +1,29 @@
-console.log('Hello World');
+var mark = {
+  fullName: 'Mark',
+  mass: 95,
+  height: 1.83,
+  bmi: function(){
+    return this.mass / (this.height * this.height);
+  }
+};
+
+var john = {
+  fullName: 'John',
+  mass: 95,
+  height: 1.83,
+  bmi: function(){
+    return this.mass / (this.height * this.height);
+  }
+};
+
+function logger(winner, loser){
+  console.log( winner.fullName + " (" + winner.bmi() + ") has a BMI higher than " + loser.fullName + " (" + loser.bmi() + ")" );
+}
+
+if (john.bmi() > mark.bmi()) {
+  logger(john, mark);
+} else if (john.bmi() < mark.bmi()) {
+  logger(mark, john);
+} else {
+  console.log("They have the same BMI: " + john.bmi() + " " + mark.bmi());
+}
